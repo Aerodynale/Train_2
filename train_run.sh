8203ef0b55a0b3ad4a2655b8666f519acc838f87
Aerodynale <abrune356@gmail.com>
2024-01-31 09:21:21 +0100

    Update train_run.sh

--- train_run.sh
+++ train_run.sh
@@ -7,7 +7,8 @@
 #$ -pe mpi 16        # cpuNumber
 #$ -l h_rt=03:00:00
 
-
+module use /software/spack/spack/share/spack/modules/linux-rocky8-sandybridge/
+module load openfoam
 
 chmod +x Allrun_cluster
 time ./Allrun_cluster > /home/meccanica/abrunelli/Train_2/output.txt
