dd71337e12c581fd2b74649eadf3e8ae4310823e
Aerodynale <abrune356@gmail.com>
2024-02-28 20:15:07 +0100

    2

--- launch_simulation.sh
+++ launch_simulation.sh
@@ -1,17 +1,17 @@
 #!/bin.bash
 #$ -cwd                 # workingDirectory
 #$ -j y
-#$ -N Train2
+#$ -N Train2bis
 #$ -S /bin/bash
 #$ -q all.q                 # queueName
-#$ -pe mpi 32        # cpuNumber
-#$ -l h_rt=10:00:00
+#$ -pe mpi 8        # cpuNumber
+#$ -l h_rt=64:00:00
 
 module use /software/spack/spack/share/spack/modules/linux-rocky8-sandybridge/
 module load openfoam
 
 # #!/bin/bash
-localDir='/global-scratch/abrunelli/Train_2'
+localDir='/global-scratch/abrunelli/Train_2bis'
 #Train case OpenFOAM
 mkdir -p logs
 cp -r 0.orig 0
@@ -19,7 +19,7 @@ cp -r 0.orig 0
 surfaceFeatureExtract > logs/01_surfaceFeature.log 2>&1
 blockMesh > logs/02_blockMesh.log 2>&1
 decomposePar > logs/03_decompose.log 2>&1
-mpirun --hostfile machinefile.$JOB_ID -np 32 snappyHexMesh -parallel -overwrite > logs/04_snappyHexMesh.log 2>&1
+mpirun --hostfile machinefile.$JOB_ID -np 8 snappyHexMesh -parallel -overwrite > logs/04_snappyHexMesh.log 2>&1
 topoSet > logs/05_topoSet.log 2>&1
 createPatch -overwrite > logs/06_createPatch.log 2>&1
 reconstructParMesh -constant > logs/07_reconstruct.log 2>&1
@@ -32,8 +32,8 @@ rm -r 0
 cp -r 0.orig 0
 renumberMesh -overwrite > logs/09_renumberMesh.log 2>&1
 decomposePar > logs/10_decomposePar_secondaIterazione.log 2>&1
-mpirun --hostfile machinefile.$JOB_ID -np 32 potentialFoam -parallel > logs/11_potentialFoam.log 2>&1
-mpirun --hostfile machinefile.$JOB_ID -np 32 simpleFoam -parallel > logs/12_simpleFoam.log 2>&1
+mpirun --hostfile machinefile.$JOB_ID -np 8 potentialFoam -parallel > logs/11_potentialFoam.log 2>&1
+mpirun --hostfile machinefile.$JOB_ID -np 8 simpleFoam -parallel > logs/12_simpleFoam.log 2>&1
 reconstructParMesh -constant > logs/13_reconstructParMesh.log 2>&1
 reconstructPar -latestTime > logs/14_reconstructPar.log 2>&1
 
